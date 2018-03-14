1de8dc349a8fdc5ed38d9773dca4ecb6dab562db
Jan Kasper <jan.kasper@students.tbs1.de>
2018-03-14 19:58:58 +0100

    add comments

---
+++ OrGate/Test_OrGate.py
@@ -0,0 +1,38 @@
+# -*- coding: utf8 -*-
+__version__ = '1.0'
+__author__ = 'Jan Kasper(jan.kasper@students.tbs1.de)'
+
+import unittest
+from OrGate import OrGate
+
+class OrGateTest(unittest.TestCase):
+    def testcase_01(self):
+        o = OrGate()
+        o.Input0 = False
+        o.Input1 = False
+        o.execute()
+        self.assertFalse(o.Output, "Class OrGate: Testcase 1 failed.")
+
+    def testcase_02(self):
+        o = OrGate()
+        o.Input0 = True
+        o.Input1 = False
+        o.execute()
+        self.assertTrue(o.Output, "Class OrGate: Testcase 2 failed.")
+
+    def testcase_03(self):
+        o = OrGate()
+        o.Input0 = False
+        o.Input1 = True
+        o.execute()
+        self.assertTrue(o.Output, "Class OrGate: Testcase 3 failed.")
+
+    def testcase_04(self):
+        o = OrGate()
+        o.Input0 = True
+        o.Input1 = True
+        o.execute()
+        self.assertTrue(o.Output, "Class OrGate: Testcase 4 failed.")
+
+if __name__ == "__main__":
+    unittest.main()
